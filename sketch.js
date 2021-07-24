3639553d6958a5ed02826bca0100f213f1fe5619
naveenpillai40 <83489511+naveenpillai40@users.noreply.github.com>
2021-07-24 11:47:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
-var fixedRect, movingRect;
-
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+var engine, world;
+var ground, ball;
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+  createCanvas(400,400);
+ engine=Engine.create()
+ world=engine.world
+var options={
+  isStatic:true
+} 
+ ground=Bodies.rectangle(200,390,200,20, options)
+ World.add(world, ground)
+ var ballOptions={
+  restitution:1
+}
+ ball=Bodies.circle(200,100,20,ballOptions)
+ World.add(world, ball)
 }
 
 function draw() {
-  background(0,0,0);  
-
-  
-  bounceOff(movingRect, fixedRect);
-  if(isTouching(movingRect, fixedRect)){
-   movingRect.shapeColor="blue"
-   fixedRect.shapeColor="blue"
-  }
-  else{
-    movingRect.shapeColor="green"
-   fixedRect.shapeColor="green"
-  }
+  background(0); 
+  Engine.update(engine)
+  rectMode(CENTER)
+  rect(ground.position.x, ground.position.y, 400, 20) 
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x, ball.position.y, 20)
   drawSprites();
 }
